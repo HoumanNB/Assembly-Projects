988e0b5c11c19c7e403a8b0b7632fc0c99478b3c
Houman Niroumand-bahramnia <HoumanNiro@protonmail.com>
2024-03-24 14:20:23 +0330

    created first.asm

--- 6502/Samples/Loop.asm
+++ 6502/Samples/Loop.asm
@@ -1,5 +1,5 @@
 ; Decreases the value from #100 to #0
     LDY #100 ; Y = 100
-loop:
+loop:   ; a name for a segment in memory
     DEY ;Y--
     BNE loop ;branch to loop if not equat to zero(repeat untill Y == 0)
