1bcb16527ca6d08905f69f10f39223fee9de0f74
Houman Niroumand-bahramnia <HoumanNiro@protonmail.com>
2024-03-24 12:05:58 +0330

    wrote some instructions

---
+++ MostPopularInstructions.asm
@@ -0,0 +1,13 @@
+LDA ; load A register
+LDX ; load X register
+LDY ; load Y register
+
+STA ; store to A register 
+STX ; store to X register 
+STY ; store to Y register 
+
+ADC ; add to accumulator(with carry)
+SBC ; subtract to accumulator(with carry)
+
+CLC ; clears the carry flag(usually before addition)
+SEC ; sets the carry flag(usually before subtraction)
