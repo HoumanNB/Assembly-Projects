3c14e41c26887346c6c37ce3a239afe315c96075
Houman Niroumand-bahramnia <HoumanNiro@protonmail.com>
2024-03-27 21:54:11 +0330

    completed Exer5

--- 6502/Samples/Exercises(1)/Exer5.asm
+++ 6502/Samples/Exercises(1)/Exer5.asm
@@ -9,16 +9,16 @@
  
 Start:
 ; TODO:
-; Load the A register with the hexadecimal value $A
-; Load the X register with the binary value %1010
+    LDA #$A     ; Load the A register with the hexadecimal value $A
+    LDX %1010   ; Load the X register with the binary value %1010
 
-; Store the value in the A register into (zero page) memory address $80
-; Store the value in the X register into (zero page) memory address $81
+    STA $80     ; Store the value in the A register into (zero page) memory address $80
+    STX $81     ; Store the value in the X register into (zero page) memory address $81
 
-; Load A with the decimal value 10
-; Add to A the value inside RAM address $80
-; Add to A the value inside RAM address $81
-; A should contain (#10 + $A + %1010) = #30 (or $1E in hexadecimal)
+    LDA #10     ; Load A with the decimal value 10
+    ADC $80     ; Add to A the value inside RAM address $80
+    ADC $81     ; Add to A the value inside RAM address $81
+                ; A should contain (#10 + $A + %1010) = #30 (or $1E in hexadecimal)
  
 ; Store the value of A into RAM position $82
  org $FFFC ; End the ROM by adding required values to memory position $FFFC
