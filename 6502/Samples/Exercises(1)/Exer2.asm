5ed3235056cb68ef9cc509e9969ca19d6bc879c9
Houman Niroumand-bahramnia <HoumanNiro@protonmail.com>
2024-03-27 20:22:38 +0330

    completed Exer2

--- 6502/Samples/Exercises(1)/Exer2.asm
+++ 6502/Samples/Exercises(1)/Exer2.asm
@@ -6,10 +6,11 @@
  
 Start:
 ; TODO:
-; Load the A register with the hexadecimal value $A
-; Load the X register with the binary value %11111111
-; Store the value in the A register into memory address $80
-; Store the value in the X register into memory address $81
+LDA #$AC        ; Load the A register with the hexadecimal value $A
+LDX %11111111   ; Load the X register with the binary value %11111111
+STA &80         ; Store the value in the A register into memory address $80
+STX &81         ; Store the value in the X register into memory address $81
+
  org $FFFC ; End the ROM by adding required values to memory position $FFFC
  .word Start ; Put 2 bytes with the reset address at memory position $FFFC
  .word Start ; Put 2 bytes with the break address at memory position $FFFE
