b167d0937df7897e87ac8f58d5d86cf5252f03f8
Houman Niroumand-bahramnia <HoumanNiro@protonmail.com>
2024-03-27 21:58:02 +0330

    completed Exer6

--- 6502/Samples/Exercises(1)/Exer6.asm
+++ 6502/Samples/Exercises(1)/Exer6.asm
@@ -5,15 +5,18 @@
  
 Start:
 ; TODO:
-; Load the A register with the decimal value 1
-; Load the X register with the decimal value 2
-; Load the Y register with the decimal value 3
-; Increment X
-; Increment Y
-; Increment A
-; Decrement X
-; Decrement Y
-; Decrement A
+    LDA #1      ; Load the A register with the decimal value 1
+    LDX #2      ; Load the X register with the decimal value 2
+    LDY #3      ; Load the Y register with the decimal value 3
+
+    INX         ; Increment X
+    INY         ; Increment Y
+    ADC #1      ; Increment A
+
+    DEX         ; Decrement X
+    DEY         ; Decrement Y
+    SBC #1      ; Decrement A
+    
  org $FFFC ; End the ROM always at position $FFFC
  .word Start ; Put 2 bytes with reset address at memory position $FFFC
  .word Start ; Put 2 bytes with break address at memory position $FFFE
