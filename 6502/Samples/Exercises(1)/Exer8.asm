a0afb2fdca04fc6249e98fc956862088009fb713
Houman Niroumand-bahramnia <HoumanNiro@protonmail.com>
2024-03-30 19:15:06 +0330

    corrected Exer8

--- 6502/Samples/Exercises(1)/Exer8.asm
+++ 6502/Samples/Exercises(1)/Exer8.asm
@@ -1,23 +1,24 @@
-;This exercise covers the increment and decrement using zero-page addressing mode.
-;The zero-page addressing mode helps us directly increment and decrement values 
-;inside memory positions. The “zero page” in the 6502 are addresses between 0 and 255. 
-;These addresses are special for the 6502 processor because we can store them using 
-;only 1 byte (8 bits), which also means they can be performed relatively fast by the CPU.
+;Your goal here is to create a loop that counts down from 10 to 0. You should also fill the
+;memory addresses from $80 to $8A with values from 0 to A
 
 ;$80 $81 $82 $83 $84 $85 $86 $87 $88 $89 $8A
 ; 0   1   2   3   4   5   6   7   8   9   A
+
 
  processor 6502
  seg Code ; Define a new segment named "Code"
  org $F000 ; Define the origin of the ROM code at memory address $F000
 
 Start:
-; TODO:
-; Load the A register with the decimal value 10
-; Store the value from A into memory position $80
+ ldy #10 ; Initialize the Y register with the decimal value 10
+
+Loop:
+ ; TODO:
+ ; Transfer Y to A
+ ; Store the value in A inside memory position $80+Y
+ ; Decrement Y
+ ; Branch back to "Loop" until we are done
  
-; Increment the value inside a (zero page) memory position $80
-; Decrement the value inside a (zero page) memory position $80
  org $FFFC ; End the ROM always at position $FFFC
  .word Start ; Put 2 bytes with reset address at memory position $FFFC
  .word Start ; Put 2 bytes with break address at memory position $FFFE
