21eea1957499f64694b98b7ef8cf18e66acf561c
Houman Niroumand-bahramnia <HoumanNiro@protonmail.com>
2024-03-30 19:47:29 +0330

    completed Exer9

--- 6502/Samples/Exercises(1)/Exer9.asm
+++ 6502/Samples/Exercises(1)/Exer9.asm
@@ -9,11 +9,12 @@
 
 Start:
  lda #1 ; Initialize the A register with the decimal value 1
+
 Loop:
 ; TODO:
-; Increment A
-; Compare the value in A with the decimal value 10
-; Branch back to loop if the comparison was not equals (to zero)
+    ADC #1          ; Increment A
+    CMP #10        ; Compare the value in A with the decimal value 10
+    BNE Loop        ; Branch back to loop if the comparison was not equals (to zero)
  
  org $FFFC ; End the ROM always at position $FFFC
  .word Start ; Put 2 bytes with reset address at memory position $FFFC
