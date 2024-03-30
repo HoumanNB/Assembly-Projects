1f86df27b28af9f9b8f30499531b788636afcd80
Houman Niroumand-bahramnia <HoumanNiro@protonmail.com>
2024-03-30 18:53:43 +0330

    completed Exer7

--- 6502/Samples/Exercises(1)/Exer7.asm
+++ 6502/Samples/Exercises(1)/Exer7.asm
@@ -10,11 +10,12 @@
 
 Start:
 ; TODO:
-; Load the A register with the decimal value 10
-; Store the value from A into memory position $80
+    LDA #10; Load the A register with the decimal value 10
+    STA $80; Store the value from A into memory position $80
 
-; Increment the value inside a (zero page) memory position $80
-; Decrement the value inside a (zero page) memory position $80
+    INC $80; Increment the value inside a (zero page) memory position $80
+    DEC $80; Decrement the value inside a (zero page) memory position $80
+    
  org $FFFC ; End the ROM always at position $FFFC
  .word Start ; Put 2 bytes with reset address at memory position $FFFC
  .word Start ; Put 2 bytes with break address at memory position $FFFE
